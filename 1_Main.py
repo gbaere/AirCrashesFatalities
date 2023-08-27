bd781d231ec9a53681110d4fbe99852bac2bdfbb
gbaere <guilherme.baere@gmail.com>
2023-08-27 17:07:41 -0300

    MVP 2.1

--- 1_Main.py
+++ 1_Main.py
@@ -1,136 +1,130 @@
+import time
 import pandas as pd
 import streamlit as st
 import custom.functions as custom
 import custom.mapas as custom_mapas
 
 st.set_page_config(
-    page_title="Aircraft accident and fatality analysis application",
+    page_title="Air accident investigation data analysis application",
     page_icon="✈️",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 
-# Carregar/processar/tratar o conjunto de dados
-def load_and_process_data():
-    dataset = pd.read_csv("dataset/air_crashes_clean.csv", sep=';', encoding="Latin-1")
+def main():
 
-    dataset['latitude_srt'] = dataset['Latitude'].astype(str)
-    dataset['longitude_srt'] = dataset['Longitude'].astype(str)
+    if "data" not in st.session_state:
+        # Exibe a barra de progresso
+        progress_bar = st.progress(0)
 
-    dataset['latitude_srt'] = dataset['latitude_srt'].apply(custom.format_coordinates)
-    dataset['longitude_srt'] = dataset['longitude_srt'].apply(custom.format_coordinates)
-
-    dataset['Latitude'] = dataset['latitude_srt']
-    dataset['Longitude'] = dataset['longitude_srt']
-
-    dataset = dataset.drop(['latitude_srt', 'longitude_srt'], axis=1)
-
-    return dataset
+        # Simulando carregamento demorado
+        for i in range(101):
+            time.sleep(0.1)  # Simula um processo demorado
+            progress_bar.progress(i)
 
 
+if __name__ == "__main__":
+    main()
 
-
-
-
-if "data" not in st.session_state:
-    st.session_state.data = load_and_process_data()
-else:
-    dataset = st.session_state.data
-
-    # Extraindo o ano
-    years = dataset['Date'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y', dayfirst=True).year)
-    min_year = years.min()
-    max_year = years.max()
-    years_values = range(min_year, max_year + 1)
-    # Sidebar filtro periodo
-    year_filter = st.sidebar.select_slider('Escolha o periodo:', options=years_values, value=(min_year, max_year))
-
-    # Sidebar filtro vitimas fatais
-    min_fatalities = dataset.Fatalities.min()
-    max_fatalities = dataset.Fatalities.max()
-    fatalities_values = range(min_fatalities, max_fatalities + 1)
-    fatalities_filter = st.sidebar.select_slider('Vitímas Fatais:', options=fatalities_values,
-                                                 value=(min_fatalities, max_fatalities))
-
-    # Obter a lista de tipos de investigação únicos
-    unique_investigation_types = dataset["Investigation Type"].unique()
-    default_investigation = unique_investigation_types[0]
-    investigation_filter = st.sidebar.multiselect("Tipo de Ocorrência:", unique_investigation_types,
-                                                  default=default_investigation)
-
-    # Sidebar filtro localidade
-    unique_country_types = sorted(dataset["Country"].dropna().unique())
-    default_country = unique_country_types[1]
-    country_filter = st.sidebar.selectbox(label="Escolha um local:",
-                                          options=unique_country_types,
-                                          index=unique_country_types.index(default_country))
-
-    # Sidebar filtro tipo de aeronaves
-    aircrafts = sorted(dataset["Aircraft Category"].unique())
-    default_country = unique_investigation_types[0]
-    # Filtrar valores vazios ou nulos e criar a lista de opções finais
-    unique_aircraft_types = [value for value in aircrafts if pd.notna(value) and value != '']
-    default_aircraft = unique_aircraft_types[0]
-    aircraft_filter = st.sidebar.multiselect("Tipo de Aeronave:", unique_aircraft_types, default=default_aircraft)
-
-    # Validandos os dados antes de filtrar
-
-    if country_filter:
-        country_condition = dataset["Country"] == country_filter
+    if "data" not in st.session_state:
+        st.session_state.data = custom.load_and_process_data()
     else:
-        country_condition = True  # Sem filtro de localidade
+        dataset = st.session_state.data
 
-    if investigation_filter:
-        investigation_condition = dataset["Investigation Type"].isin(investigation_filter)
-    else:
-        investigation_condition = True  # Sem filtro de investigação
+        # Extraindo o ano
+        years = dataset['Date'].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y', dayfirst=True).year)
+        min_year = years.min()
+        max_year = years.max()
+        years_values = range(min_year, max_year + 1)
+        # Sidebar filtro periodo
+        year_filter = st.sidebar.select_slider('Escolha o periodo:', options=years_values, value=(min_year, max_year))
 
-    if aircraft_filter:
-        aircraft_condition = dataset["Aircraft Category"].isin(aircraft_filter)
-    else:
-        aircraft_condition = True  # Sem filtro de categoria de aeronaves
+        # Sidebar filtro vitimas fatais
+        min_fatalities = dataset.Fatalities.min()
+        max_fatalities = dataset.Fatalities.max()
+        fatalities_values = range(min_fatalities, max_fatalities + 1)
+        fatalities_filter = st.sidebar.select_slider('Vitímas Fatais:', options=fatalities_values,
+                                                     value=(min_fatalities, max_fatalities))
 
-    # Aplicando todas as condições de filtro
-    year_condition = ((pd.DatetimeIndex(dataset['Date']).year >= year_filter[0]) &
-                      (pd.DatetimeIndex(dataset['Date']).year <= year_filter[1]))
+        # Obter a lista de tipos de investigação únicos
+        unique_investigation_types = dataset["Investigation Type"].unique()
+        default_investigation = unique_investigation_types[0]
+        investigation_filter = st.sidebar.multiselect("Tipo de Ocorrência:", unique_investigation_types,
+                                                      default=default_investigation)
 
-    fatalities_condition = ((dataset['Fatalities'] >= fatalities_filter[0]) &
-                            (dataset['Fatalities'] <= fatalities_filter[1]))
+        # Sidebar filtro localidade
+        unique_country_types = sorted(dataset["Country"].dropna().unique())
+        default_country = unique_country_types[0]
+        country_filter = st.sidebar.selectbox(label="Escolha um local:",
+                                              options=unique_country_types,
+                                              index=unique_country_types.index(default_country))
 
-    final_condition = year_condition & fatalities_condition & investigation_condition & aircraft_condition & country_condition
+        # Sidebar filtro tipo de aeronaves
+        aircrafts = sorted(dataset["Aircraft Category"].unique())
+        default_country = unique_investigation_types[0]
+        # Filtrar valores vazios ou nulos e criar a lista de opções finais
+        unique_aircraft_types = [value for value in aircrafts if pd.notna(value) and value != '']
+        default_aircraft = unique_aircraft_types[0]
+        aircraft_filter = st.sidebar.multiselect("Tipo de Aeronave:", unique_aircraft_types, default=default_aircraft)
 
-    if final_condition.any():
-        dataset_filtered = dataset[final_condition]
-    else:
-        dataset_filtered = dataset.copy()
+        # Validandos os dados antes de filtrar
 
-    st.write(dataset_filtered)
+        if country_filter:
+            country_condition = dataset["Country"] == country_filter
+        else:
+            country_condition = True  # Sem filtro de localidade
 
-    if not dataset_filtered.empty:
-        # Filtrar as linhas com Latitude e Longitude diferentes de 0, não vazias e não NaN
-        dataset_filtered = dataset_filtered[
-            ~(
-                    (dataset_filtered['Latitude'] == "00.0") |
-                    (dataset_filtered['Longitude'] == "00.0") |
-                    dataset_filtered['Latitude'].isna() |
-                    dataset_filtered['Longitude'].isna()
-            )
-        ]
+        if investigation_filter:
+            investigation_condition = dataset["Investigation Type"].isin(investigation_filter)
+        else:
+            investigation_condition = True  # Sem filtro de investigação
 
-    # Exibir o gráfico
-    custom_mapas.analise_acidentes_plotly(dataset_filtered)
+        if aircraft_filter:
+            aircraft_condition = dataset["Aircraft Category"].isin(aircraft_filter)
+        else:
+            aircraft_condition = True  # Sem filtro de categoria de aeronaves
 
-    custom_mapas.analise_fatalidade_e_lesoes(dataset_filtered)
+        # Aplicando todas as condições de filtro
+        year_condition = ((pd.DatetimeIndex(dataset['Date']).year >= year_filter[0]) &
+                          (pd.DatetimeIndex(dataset['Date']).year <= year_filter[1]))
 
-    custom_mapas.analise_aeronaves(dataset_filtered)
+        fatalities_condition = ((dataset['Fatalities'] >= fatalities_filter[0]) &
+                                (dataset['Fatalities'] <= fatalities_filter[1]))
 
-    tab1, tab2 = st.tabs(["Fabricantes", "Detalhes"])
+        final_condition = year_condition & fatalities_condition & investigation_condition & aircraft_condition & country_condition
 
-    with tab1:
-        custom_mapas.analise_fabricante_aeronaves(dataset_filtered)
-    with tab2:
-        custom_mapas.analise_fabricante_aeronaves_detalhes_tabela(dataset_filtered)
+        if final_condition.any():
+            dataset_filtered = dataset[final_condition]
+        else:
+            dataset_filtered = dataset.copy()
 
+        st.write(dataset_filtered)
 
-    # Link para o GitHub
-    st.sidebar.markdown("[GitHub Repository](https://github.com/gbaere)")
+        if not dataset_filtered.empty:
+            # Filtrar as linhas com Latitude e Longitude diferentes de 0, não vazias e não NaN
+            dataset_filtered = dataset_filtered[
+                ~(
+                        (dataset_filtered['Latitude'] == "00.0") |
+                        (dataset_filtered['Longitude'] == "00.0") |
+                        dataset_filtered['Latitude'].isna() |
+                        dataset_filtered['Longitude'].isna()
+                )
+            ]
+
+        # Exibir o gráfico
+        custom_mapas.analise_acidentes_plotly(dataset_filtered)
+
+        custom_mapas.analise_fatalidade_e_lesoes(dataset_filtered)
+
+        custom_mapas.analise_aeronaves(dataset_filtered)
+
+        tab1, tab2 = st.tabs(["Fabricantes", "Detalhes"])
+
+        with tab1:
+            custom_mapas.analise_fabricante_aeronaves(dataset_filtered)
+        with tab2:
+            custom_mapas.analise_fabricante_aeronaves_detalhes_tabela(dataset_filtered)
+
+        # Link para o GitHub
+        st.sidebar.markdown("[GitHub Repository](https://github.com/gbaere)")
--- custom/functions.py
+++ custom/functions.py
@@ -1,63 +1,20 @@
-from typing import Optional, Tuple
-from plotly.figure_factory._county_choropleth import shapely
+import pandas as pd
 
+def load_and_process_data():
+    dataset = pd.read_csv("dataset/air_crashes_clean.csv", sep=';', encoding="Latin-1")
 
-# it would be used like:
-# lon_lat = valid_lonlat(lon, lat)
-# if lon_lat is None:
-#     raise ValueError("(lon, lat) is not in WGS84 bounds")
-# else:
-#     lon, lat = lon_lat
-def valid_lonlat(lon: float, lat: float) -> Optional[Tuple[float, float]]:
-    """
-    This validates a lat and lon point can be located
-    in the bounds of the WGS84 CRS, after wrapping the
-    longitude value within [-180, 180)
+    dataset['latitude_srt'] = dataset['Latitude'].astype(str)
+    dataset['longitude_srt'] = dataset['Longitude'].astype(str)
 
-    :param lon: a longitude value
-    :param lat: a latitude value
-    :return: (lon, lat) if valid, None otherwise
-    """
-    if not isinstance(lon, (int, float)) or not isinstance(lat, (int, float)):
-        return None  # Retornar None para valores inválidos
+    dataset['latitude_srt'] = dataset['latitude_srt'].apply(format_coordinates)
+    dataset['longitude_srt'] = dataset['longitude_srt'].apply(format_coordinates)
 
-    if lon < -180 or lon > 180 or lat < -90 or lat > 90:
-        return None  # Retornar None para valores fora dos intervalos válidos
+    dataset['Latitude'] = dataset['latitude_srt']
+    dataset['Longitude'] = dataset['longitude_srt']
 
-    # Put the longitude in the range of [0,360):
-    lon %= 360
-    # Put the longitude in the range of [-180,180):
-    if lon >= 180:
-        lon -= 360
-    lon_lat_point = shapely.geometry.Point(lon, lat)
-    lon_lat_bounds = shapely.geometry.Polygon.from_bounds(
-        xmin=-180.0, ymin=-90.0, xmax=180.0, ymax=90.0
-    )
-    # return lon_lat_bounds.intersects(lon_lat_point)
-    # would not provide any corrected values
+    dataset = dataset.drop(['latitude_srt', 'longitude_srt'], axis=1)
 
-    # ... (mesmo código)
-    if lon_lat_bounds.intersects(lon_lat_point):
-        return lon, lat  # Retorna a tupla (longitude, latitude)
-    else:
-        return None  # Retorna None se não for válido
-
-
-def convert_value(value):
-    if isinstance(value, str):
-        return float(value.replace(',', ''))
-    return value
-
-
-
-def format_latitude(number_str):
-    return number_str.replace('.', '')[:-2] + '.' + number_str[-2:]
-
-def format_longitude(number_str):
-    if number_str[0] == '-':
-        return number_str[:4] + '.' + number_str[4:]
-    else:
-        return number_str[:3] + '.' + number_str[3:]
+    return dataset
 
 
 def format_coordinates(coord_str):
