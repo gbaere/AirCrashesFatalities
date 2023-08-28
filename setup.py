aba450ddbb297bf097b6881439c14ce025966967
gbaere <guilherme.baere@gmail.com>
2023-08-28 17:44:34 -0300

    Update Requeriments.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AirCrashes-app',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Guilherme Bender',
+    author_email='guilherme.baere@gmail.com',
+    description='App to analyze and visualize air accident investigation data in the world'
+)
